c8f60fd3a65617acfc7d700da59fe91ef2249cab
garibelp <gabriel.paiv.oliv@hotmail.com>
2023-03-05 19:04:35 -0300

    [TKD-036] - Updated name filter to work with case insensitive

--- src/main/java/br/com/extratora/twelvekingdoms/repository/SheetRepository.java
+++ src/main/java/br/com/extratora/twelvekingdoms/repository/SheetRepository.java
@@ -22,16 +22,16 @@ public interface SheetRepository extends JpaRepository<SheetModel, UUID> {
     String SHEET_DETAILS_BASE_SELECT = "select new br.com.extratora.twelvekingdoms.dto.CampaignSheetDto" +
             "(s.id, s.name, s.level, s.mentalCurrent, s.mentalTotal, s.physicalCurrent, s.physicalTotal, s.lineage.name, s.background.name)";
 
-    @Query(value = SHEET_PAGINATED_BASE_SELECT + " from SheetModel s where s.player.id = ?1 and s.isActive = true and s.name like %?2%",
-            countQuery = "select count(*) from SheetModel s where s.player.id = ?1 and s.isActive = true and s.name like %?2%")
+    @Query(value = SHEET_PAGINATED_BASE_SELECT + " from SheetModel s where s.player.id = ?1 and s.isActive = true and lower(s.name) like lower(concat('%', ?2,'%'))",
+            countQuery = "select count(*) from SheetModel s where s.player.id = ?1 and s.isActive = true and lower(s.name) like lower(concat('%', ?2,'%'))")
     Page<BasicSheetDto> findActivePlayerSheetsPaginatedFilterByNameOpt(Pageable pageable, UUID playerId, String name);
 
-    @Query(value = SHEET_PAGINATED_BASE_SELECT + " from SheetModel s where s.isActive = true and s.name like %?1%",
-            countQuery = "select count(*) from SheetModel s where s.isActive = true and s.name like %?1%")
+    @Query(value = SHEET_PAGINATED_BASE_SELECT + " from SheetModel s where s.isActive = true and lower(s.name) like lower(concat('%', ?1,'%'))",
+            countQuery = "select count(*) from SheetModel s where s.isActive = true and lower(s.name) like lower(concat('%', ?1,'%'))")
     Page<BasicSheetDto> findActiveSheetsPaginatedFilterByNameOpt(Pageable pageable, String name);
 
-    @Query(value = SHEET_PAGINATED_BASE_SELECT + " from SheetModel s where s.name like %?1%",
-            countQuery = "select count(*) from SheetModel s where s.name like %?1%")
+    @Query(value = SHEET_PAGINATED_BASE_SELECT + " from SheetModel s where lower(s.name) like lower(concat('%', ?1,'%'))",
+            countQuery = "select count(*) from SheetModel s where lower(s.name) like lower(concat('%', ?1,'%'))")
     Page<BasicSheetDto> findSheetsPaginatedFilterByNameOpt(Pageable pageable, String name);
 
     @Query(value = "select s from SheetModel s inner join fetch s.aptitudes where s.id = ?1")
