ae790a414e2fdd22dae6176abed342edb0e4591e
garibelp <gabriel.paiv.oliv@hotmail.com>
2023-07-02 16:28:03 -0300

    [TKD-040] - Fix of swapped skills

---
+++ src/main/resources/db/migration/V1_2_1__fix_of_skills.sql
@@ -0,0 +1,11 @@
+
+update skills
+set
+    name = 'Ocultação',
+    description = 'O Ladino pode usar sua Celeridade e gastar 2 Pontos Energia Física para tornar-se Oculto, praticamente invisível no campo de batalha, antes ou após realizar seu ataque, como uma Ação Simples. Até se mover ou atacar novamente, o Ladino não pode ser alvo de ataques ou habilidades direcionados.',
+    cost = 2
+where id = '1f49c878-8e5d-45ac-afc8-5537b28936fc';
+
+update skills
+set description = 'O Ladino tem um talento especial para ir e vir despercebido. Desde que tome um pouco mais de cuidado, o Ladino é capaz de gastar uma Ação para fazer um teste de Celeridade com 1 Ponto de Energia Física, independente do acerto, e adicional e tornar seu rosto quase completamente imemorável. Em um lugar relacionado a seu Antecedente, ele é capaz de estender esse efeito a até 4 pessoas. Caso cause algum tipo de alarde, o Ladino perde esse efeito imediatamente, atraindo a atenção de todos ao redor. Apenas tome cuidado: Bárbaros ainda podem perceber que Algo de Errado Não Está Certo!'
+where id = '2df6ae6e-f1ac-4c60-958f-25802b034bc4';
